bd6230e0eaa4fb57d91079f9db9b5aabd7f7f668
Den Shelupets <shelupets1992@gmail.com>
2021-10-09 16:03:46 +0300

    update customer

--- src/main/java/com/fastbobby/fastbobbygo/customers/repository/CustomerRepository.java
+++ src/main/java/com/fastbobby/fastbobbygo/customers/repository/CustomerRepository.java
@@ -1,13 +1,2 @@
-package com.fastbobby.fastbobbygo.customers.repository;
-
-import com.fastbobby.fastbobbygo.customers.model.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-/**
- * Created by Shelupets Denys on 09.10.2021.
- */
-@Repository
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-
+package com.fastbobby.fastbobbygo.customers.repository;public interface CustomerRepository {
 }
